0e279db417b3e8ed294b7baee5fd6ceabb6c1162
Michael <mielliott@ufl.edu>
2026-03-07 18:18:59 -0500

    Upgrade SDK to 0.2.6

--- src/agent.py
+++ src/agent.py
@@ -1,13 +1,15 @@
 from typing import override, Optional
 
+import dotenv
 from ichatbio.agent import IChatBioAgent
-from ichatbio.agent_response import ResponseContext, IChatBioAgentProcess
+from ichatbio.agent_response import ResponseContext
 from ichatbio.server import build_agent_app
-from ichatbio.types import AgentCard, AgentEntrypoint
+from ichatbio.types import AgentCard
 from pydantic import BaseModel
 from starlette.applications import Starlette
 
 from entrypoints import search_inaturalist_observations
+
 
 class INaturalistAgent(IChatBioAgent):
     @override
@@ -15,6 +17,7 @@ class INaturalistAgent(IChatBioAgent):
         return AgentCard(
             name="iNaturalist Observation Search",
             description="Searches for observations in iNaturalist (https://inaturalist.org).",
+            documentation_url="https://github.com/acislab/ichatbio-inaturalist-agent",
             icon='https://static.inaturalist.org/wiki_page_attachments/3154-medium.png',
             entrypoints=[
                 search_inaturalist_observations.entrypoint
@@ -30,8 +33,8 @@ class INaturalistAgent(IChatBioAgent):
                 raise ValueError()
 
 
-
 def create_app() -> Starlette:
+    dotenv.load_dotenv()
     agent = INaturalistAgent()
     app = build_agent_app(agent)
     return app
